9da4289f4983e17a1c4be02729c12ad036685bf8
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-06-01 14:45:00 +0200

    instanciate all classes

--- src/test.cpp
+++ src/test.cpp
@@ -11,28 +11,43 @@ void simple_loading_example(int argc, char **argv)
     std::string LIBRARY_2="libplugin_fantasy.so";
     try
     {
-      class_loader::ClassLoader loader1(LIBRARY_1, false);
-      loader1.createInstance<Base>("Cat")->saySomething();
+        class_loader::ClassLoader loader1(LIBRARY_1, false);
+        std::vector<std::string> classes = loader1.getAvailableClasses<Base>();
+        for(unsigned int c = 0; c < classes.size(); ++c)
+        {
+            boost::shared_ptr<Base> plugin = loader1.createInstance<Base>(classes[c]);
+            plugin->saySomething();
+        }
     }
     catch(class_loader::ClassLoaderException& e)
     {
       std::cout << "ClassLoaderException: " << e.what() << "\n";
+    }
+    catch (class_loader::CreateClassException& e)
+    {
+        std::cout <<"CreateClassException: "<< e.what() << "\n";
     }
 
 
     try
     {
-      class_loader::ClassLoader loader2(LIBRARY_2, false);
-      loader2.createInstance<Base>("Monster")->saySomething();
+        class_loader::ClassLoader loader2(LIBRARY_2, false);
+        std::vector<std::string> classes = loader2.getAvailableClasses<Base>();
+        for(unsigned int c = 0; c < classes.size(); ++c)
+        {
+            boost::shared_ptr<Base> plugin = loader2.createInstance<Base>(classes[c]);
+            plugin->saySomething();
+        }
+
     }
     catch(class_loader::ClassLoaderException& e)
     {
       std::cout << "ClassLoaderException: " << e.what() << "\n";
     }
-
-
-
-
+    catch (class_loader::CreateClassException& e)
+    {
+        std::cout <<"CreateClassException: "<< e.what() << "\n";
+    }
 }
 
 
