966c7f82c11c11c7a5c543e0f090a502ec3bfe4c
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-09-29 18:36:13 +0200

    update envire example

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <class_loader/class_loader.h>
 #include "base.h"
+#include <envire_core/Environment.hpp>
 
 
 void simple_loading_example(int argc, char **argv)
@@ -11,12 +12,17 @@ void simple_loading_example(int argc, char **argv)
     std::string LIBRARY_2="libplugin_fantasy.so";
     try
     {
+        envire::core::Environment envire1;
+
         class_loader::ClassLoader loader1(LIBRARY_1, false);
         std::vector<std::string> classes = loader1.getAvailableClasses<Base>();
         for(unsigned int c = 0; c < classes.size(); ++c)
         {
-            boost::shared_ptr<Base> plugin = loader1.createInstance<Base>(classes[c]);
-            plugin->saySomething();
+            envire::core::Item<Base> item;
+
+            item->value = loader1.createInstance<Base>(classes[c]);
+            item->value->saySomething();
+            envire1.insert(boost::uuids::random_generator(), new envire::core::Environment());
         }
     }
     catch(class_loader::ClassLoaderException& e)
